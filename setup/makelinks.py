dbd282129d1e701952f5c77e87682036f7465ce2
agirardeaudale <agirardeaudale@users.noreply.github.com>
2016-01-14 20:10:04 -0800

    Simplify makelinkes.py, move list of linked files to links.json

--- setup/makelinks.py
+++ setup/makelinks.py
@@ -1,177 +1,42 @@
 #!/usr/bin/env python
-""" Andrew Girardeau-Dale
-8/22/14
-
-"""
-
-################################## Imports ####################################
 
 import os
-import sys
-import argparse
-try:
-    import ipdb as pdb
-except ImportError:
-    import pdb
+import json
 
+HOME = os.path.expanduser("~")
+LINKFILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "links.json"))
 
-################################# Utility ###################################
+def make_link(source, target):
+    target = target.replace("~", HOME, 1)
+    source = source.replace("~", HOME, 1)
+    (source_directory, _) = os.path.split(source)
 
-def englishJoin(stringList):
-    """Join a list of strings using commas, spaces, and the word "and", as one
-    would in English. Oxford comma is included.
+    if not os.path.exists(target):
+        print("No file to link to at {0}".format(target))
+        return
 
-    """
+    try:
+        os.makedirs(source_directory)
+    except:
+        pass
 
-    if len(stringList) >= 2:
-        return "{0}, and {1}".format(", ".join(stringList[:-1]), stringList[-1])
-    elif len(stringList) == 1:
-        return stringList[0];
-    else:
-        return "[]"
-
-
-################################# Constants ###################################
-
-files = {
-    "vim" :
-        [
-            {
-                "source" : "~/dotfiles/vim/vimrc",
-                "link"   : "~/.vimrc"
-            },
-        ],
-
-    "git" :
-        [
-            {
-                "source" : "~/dotfiles/git/gitconfig",
-                "link"   : "~/.gitconfig"
-            },
-            {
-                "source" : "~/dotfiles/git/gitignore",
-                "link"   : "~/.gitignore"
-            },
-        ],
-
-    "bash" :
-        [
-            {
-                "source" : "~/dotfiles/bash/bashrc",
-                "link"   : "~/.bashrc"
-            },
-            {
-                "source" : "~/dotfiles/bash/bash_aliases",
-                "link"   : "~/.bash_aliases"
-            },
-            {
-                "source" : "~/dotfiles/bash/dircolors",
-                "link"   : "~/.dircolors"
-            },
-        ],
-
-    "python" :
-        [
-            {
-                "source" : "~/dotfiles/python/ipython_config.py",
-                "link"   : "~/.ipython/profile_default/ipython_config.py"
-            },
-        ],
-
-    "tmux" :
-        [
-            {
-                "source" : "~/dotfiles/tmux/tmux.conf",
-                "link"   : "~/.tmux.conf"
-            },
-        ],
-
-    "scripts" :
-        [
-            {
-                "source" : "~/dotfiles/scripts/ctagdisplay.py",
-                "link"   : "~/bin/ctagdisplay"
-            },
-            {
-                "source" : "~/dotfiles/scripts/gitlog.sh",
-                "link"   : "~/bin/gitlog"
-            },
-        ],
-
-}
-
-potentialGroups = files.keys()
-potentialGroupString = englishJoin(['"{0}"'.format(x) for x in potentialGroups])
-
-home = os.path.expanduser("~")
-
-################################# Functions ###################################
-
-def createLinks(fileList):
-    for file in fileList:
-        source = file["source"].replace("~", home, 1)
-        link = file["link"].replace("~", home, 1)
-        (linkDirectory, _) = os.path.split(link)
+    if os.path.lexists(source):
         try:
-            os.makedirs(linkDirectory)
-        except:
-            pass
-        if os.path.lexists(link):
-            try:
-                os.unlink(link)
-            except OSError as e:
-                print("Couldn't remove existing link at {0}".format(link, str(e)))
-
-        try:
-            os.symlink(source, link)
+            os.unlink(source)
         except OSError as e:
-            print("Error linking {0} to {1}: {2}".format(link, source, str(e)))
+            print("Couldn't remove existing link at {0}: {1}"
+                    .format(source, str(e)))
+            return
 
-
-################################ Control FLow #################################
-
-def getArgumentParser():
-    """Construct a command line argument parser.
-    
-    Returns:
-        argParser (argparse.ArgumentParser).
-    
-    """
-
-    argParser = argparse.ArgumentParser(
-            prog="Make links in home directory to dotfiles in repo folder")
-    
-    helpString = ("Groups of dotfiles to create links for. Options are {0}. "
-            "If left blank, all groups will be linked."
-            ).format(potentialGroupString)
-    argParser.add_argument("groups", nargs="*", metavar="groups",
-            help=helpString)
-    
-    argParser.add_argument("-q", "--quiet", const=True, default=False,
-            nargs="?", metavar="quiet mode", dest="quiet",
-            help="Suppress program status printing.")
-
-    return argParser
-
-#################################### Main #####################################
-
-def main(argList):
-
-    argParser = getArgumentParser()
-    args = argParser.parse_args(argList)
-
-    groups = []
-    if not args.groups:
-        groups = potentialGroups
-    else:
-        groups = args.groups
-        for group in groups:
-            if group not in potentialGroups:
-                raise ValueError('Group "{0}" not recognized. Options are {1}.'
-                        .format(group, potentialGroupString))
-    
-    for group in groups:
-        createLinks(files[group])
+    try:
+        os.symlink(target, source)
+    except OSError as e:
+        print("Error linking {0} to {1}: {2}"
+                .format(source, target, str(e)))
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    with open(LINKFILE, "r") as f:
+        link_data = json.load(f)
+
+    for target, source in link_data:
+        make_link(source, target)
