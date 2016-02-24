7ae9d9d62b66d57be9ea918a9c2546d2054881bf
agirardeaudale <agirardeaudale@users.noreply.github.com>
2016-02-23 17:14:08 -0800

    Remove shebang from newontainer.sh

--- scripts/newcontainer.sh
+++ scripts/newcontainer.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env sh
-# Open a terminal in a new i3 container and 
+# Open a terminal in a new i3 container and have it execute the given commands
 wd=`pwd`
 run_in_directory_executable=`which run-in-directory`
 i3-msg exec "i3-sensible-terminal -e '$run_in_directory_executable $wd $1'"
