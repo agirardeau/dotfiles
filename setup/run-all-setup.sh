f76c7d578055476bbab6c1aa01c09e1cebcdad2c
Andrew Girardeau-Dale <agirardeaudale@gmail.com>
2019-06-14 14:44:55 -0600

    Add misc-setup.sh to run-all-setup.sh

--- setup/run-all-setup.sh
+++ setup/run-all-setup.sh
@@ -3,3 +3,7 @@ directory=`dirname $0`
 echo "==================================="
 echo "Running dotfile-link-setup.py..."
 $directory/dotfile-link-setup.py
+
+echo "==================================="
+echo "Running misc-setup..."
+$directory/misc-setup
