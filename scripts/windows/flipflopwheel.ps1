293582dcbf42bb00146636b84dbda02890e911f1
Andrew Girardeau-Dale <26529953+agirardeau@users.noreply.github.com>
2024-12-27 11:32:16 -0800

    Add instructions to flipflopwheel script

--- scripts/windows/flipflopwheel.ps1
+++ scripts/windows/flipflopwheel.ps1
@@ -1,1 +1,2 @@
+# Run this in powershell as administrator, then restart computer
 Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
